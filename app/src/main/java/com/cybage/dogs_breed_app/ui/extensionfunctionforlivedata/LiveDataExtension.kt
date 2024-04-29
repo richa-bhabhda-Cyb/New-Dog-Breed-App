98bca1599c181a448dcd8a6f5cf756d132eea598
richa_bhabhda_Cyb <richabh@cybage.com>
2024-04-29 13:00:09 +0530

    changes done to DogListScreen

--- app/src/main/java/com/cybage/dogs_breed_app/ui/extensionfunctionforlivedata/LiveDataExtension.kt
+++ app/src/main/java/com/cybage/dogs_breed_app/ui/extensionfunctionforlivedata/LiveDataExtension.kt
@@ -1,4 +1,15 @@
 package com.cybage.dogs_breed_app.ui.extensionfunctionforlivedata
 
-class LiveDataExtension {
+// LiveDataExtensions.kt
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LiveData
+
+fun LiveData<List<String>>.collectAsState(): State<List<String>> {
+    val state = mutableStateOf<List<String>>(emptyList())
+    observeForever { list ->
+        state.value = list
+    }
+    return state
 }
--- app/src/main/java/com/cybage/dogs_breed_app/ui/screen/DogListScreen.kt
+++ app/src/main/java/com/cybage/dogs_breed_app/ui/screen/DogListScreen.kt
@@ -79,12 +79,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -92,12 +92,26 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cybage.dogs_breed_app.ui.extensionfunctionforlivedata.collectAsState
 import com.cybage.dogs_breed_app.viewmodel.DogViewModel
+
+
+//@Composable
+//fun DogListScreen(viewModel: DogViewModel = viewModel()) {
+//    val dogBreeds = viewModel.dogBreeds.value ?: emptyList()
+//
+//    LazyColumn {
+//        items(dogBreeds) { breed ->
+//            DogBreedItem(breed = breed)
+//        }
+//    }
+//}
+
 
 
 @Composable
 fun DogListScreen(viewModel: DogViewModel = viewModel()) {
-    val dogBreeds by viewModel.dogBreeds.collectAsState(initial = emptyList())
+    val dogBreeds by viewModel.dogBreeds.collectAsState()
 
     LazyColumn {
         items(dogBreeds) { breed ->
